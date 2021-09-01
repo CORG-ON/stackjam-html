f01c1f70a7e9d5d0c93b3ea1af6fdd33645e0fc2
Jonathan Wu <jonathan.wu3@outlook.com>
2021-09-01 09:56:06 -0400

    fix animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll", () => {
   for (let el of document.querySelectorAll(".fade-in-3-pre")) {
-    if (el.getBoundingClientRect().top - window.innerHeight < -200) {
+    if (el.getBoundingClientRect().top - window.innerHeight < -100) {
       el.classList.remove("fade-in-3-pre");
       el.classList.add("fade-in-3");
     }
