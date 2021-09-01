2ca133e57621b035e1f1c29ed356b4cb676cb303
Jonathan Wu <jonathan.wu3@outlook.com>
2021-09-01 09:54:18 -0400

    fix meet the team page

--- animation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", () => {
-  for (let el of document.querySelectorAll(".fade-in-3-pre")) {
-    if (el.getBoundingClientRect().top - window.innerHeight < -200) {
-      el.classList.remove("fade-in-3-pre");
-      el.classList.add("fade-in-3");
-    }
-  }
-});
